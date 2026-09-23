16887c873516e1b362cc56e776a04b944a2a371e
Sawaira-Iftikhar <isawairaiftikhar@gmail.com>
2026-09-23 22:49:03 +0500

    .get() method VS Direct Brachets

--- 02_Dictionary_Methods.py
+++ 02_Dictionary_Methods.py
@@ -11,3 +11,11 @@
 # ==========================================
 #  PART A: RETRIEVAL & VIEWS 
 # ==========================================
+
+# Q1. `.get()` METHOD vs DIRECT BRACKETS:
+#     Given: person = {"name": "Bilal", "city": "Karachi"}
+#     a) Access "name" using .get()
+#     b) Access "salary" using .get() without default (prints None)
+#     c) Access "salary" using .get("salary", 50000)
+#     d) Explain why .get() is safer than person["salary"]
+
