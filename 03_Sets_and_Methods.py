eec285471c27e76c21c722b9ba05d05350e5329c
Sawaira-Iftikhar <isawairaiftikhar@gmail.com>
2026-09-24 22:31:49 +0500

    Q3 add and update method

--- 03_Sets_and_Methods.py
+++ 03_Sets_and_Methods.py
@@ -67,3 +67,10 @@ print("Length: ",len(clean_list))
 # ==========================================
 #  PART B: SET METHODS 
 # ==========================================
+
+# Q3. `.add()` vs `.update()`:
+#     Given: my_set = {10, 20, 30}
+#     a) Add a single element 40 using .add()
+#     b) Add multiple elements [50, 60, 70] using .update()
+#     c) What happens if you try my_set.add([80, 90])? Write the error.
+#     d) Print my_set after adding and updating.
