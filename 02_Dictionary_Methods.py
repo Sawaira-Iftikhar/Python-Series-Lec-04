556bc1c51291f2922872186cb773af1b7cfd7f2f
Sawaira-Iftikhar <isawairaiftikhar@gmail.com>
2026-09-24 16:35:32 +0500

    Q5 update method

--- 02_Dictionary_Methods.py
+++ 02_Dictionary_Methods.py
@@ -108,3 +108,10 @@ print("Readings:", readings)
 # ==========================================
 #  PART B: UPDATES & REMOVALS 
 # ==========================================
+
+# Q5. `.update()` METHOD:
+#     Given: profile = {"name": "Mariam", "role": "Junior Dev"}
+#     a) Update "role" to "Senior Dev" and add "tech": "Python" using one .update()
+#     b) Update profile using another dict: {"location": "Remote", "salary": 90000}
+#     c) Print the final profile
+
