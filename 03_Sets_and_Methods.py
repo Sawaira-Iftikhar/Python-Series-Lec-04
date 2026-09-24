b26e53705efdc2bc15ac0523f820e67d8572d758
Sawaira-Iftikhar <isawairaiftikhar@gmail.com>
2026-09-24 22:02:35 +0500

    add top details of File 3: Sets and Methods

---
+++ 03_Sets_and_Methods.py
@@ -0,0 +1,8 @@
+"""
+============================================
+  LECTURE 4 - FILE 3: SETS & SET METHODS
+  Topics: Set Basics, Uniqueness, Set Methods,
+          Mathematical Set Operations
+  Total Questions: 
+============================================
+"""
