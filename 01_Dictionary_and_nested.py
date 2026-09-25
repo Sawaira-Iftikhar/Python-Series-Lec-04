36f45eb01ba476fb25dfecfc9a27842c95919368
Sawaira-Iftikhar <isawairaiftikhar@gmail.com>
2026-09-25 22:49:37 +0500

    add 0 in total quesitons of top detail section

--- 01_Dictionary_and_nested.py
+++ 01_Dictionary_and_nested.py
@@ -3,7 +3,7 @@
   LECTURE 4 - FILE 1: DICTIONARY & NESTED DICT
   Topics: Dict Creation, Hashable Key Rules,
           Access, Modifying, Nested Dictionaries
-  Total Questions: 8
+  Total Questions: 08
 ============================================
 
 """
