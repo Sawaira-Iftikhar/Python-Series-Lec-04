756fac3c3d33c1fa79341cc1189db9ac72b54e18
Sawaira-Iftikhar <isawairaiftikhar@gmail.com>
2026-09-25 22:48:09 +0500

    add total challenge no in top details

--- Challenge.py
+++ Challenge.py
@@ -2,7 +2,7 @@
 ============================================
   LECTURE 4 - FILE 4: BOSS CHALLENGE 
   Topics: Dictionaries, Nested Dicts & Sets
-  Total Challenges: 
+  Total Challenges: 03
 ============================================
 """
 
